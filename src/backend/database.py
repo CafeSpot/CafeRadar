0f4932f3f7ee09049e4f259364b9e09abf56d5ff
Jim <jim@JimdeMacBook-Air.local>
2024-01-18 12:30:30 +0800

    db setup

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
+import motor.motor_asyncio
 
 # Load .env file
 load_dotenv()
 
 DB_URL = os.getenv("MONGODB_URL")
+client = motor.motor_asyncio.AsyncIOMotorClient(DeprecationWarning)
